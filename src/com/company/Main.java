12d0c5fa6bd21a229d86a1662b703a2db47c0506
Ralph F <RaftsDev@gmail.com>
2020-09-08 12:04:19 -0400

    Feat: Moved Printing available options to separate method.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -28,17 +28,23 @@ public class Main {
 
         boolean quit = false;
         while(!quit){
-            System.out.println("----------- List of available options --------");
-            System.out.println("0 - create a Bank\n" +
-                               "1 - create a Branch\n" +
-                               "2 - create a Customer\n" +
-                               "3 - create a Transaction of customer\n" +
-                               "4 - Show a Bank\n" +
-                               "5 - Show a Branches\n" +
-                               "6 - Show a Customers of Branch\n" +
-                               "7 - Show a Customers of Branch w transactions\n" +
-                               "8 - Exit of application");
+
+            printOptions();
+
             quit = true;//just for test
         }
+    }
+
+    private static void printOptions(){
+        System.out.println("----------- List of available options --------");
+        System.out.println("0 - create a Bank\n" +
+                "1 - create a Branch\n" +
+                "2 - create a Customer\n" +
+                "3 - create a Transaction of customer\n" +
+                "4 - Show a Bank\n" +
+                "5 - Show a Branches\n" +
+                "6 - Show a Customers of Branch\n" +
+                "7 - Show a Customers of Branch w transactions\n" +
+                "8 - Exit of application");
     }
 }
