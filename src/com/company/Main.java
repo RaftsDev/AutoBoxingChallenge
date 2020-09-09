7a804104f6cb3596b454582c4bf83806cdd3dad9
Ralph F <RaftsDev@gmail.com>
2020-09-09 00:06:15 -0400

    Feat: Implemented switch operator.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -36,7 +36,38 @@ public class Main {
             int action = printOptions();
             System.out.println("action:"+action);
 
-            quit = true;//just for test
+            switch(action){
+                case 0:
+                    System.out.println("You pressed 0");
+                    break;
+                case 1:
+                    System.out.println("You pressed 1");
+                    break;
+                case 2:
+                    System.out.println("You pressed 2");
+                    break;
+                case 3:
+                    System.out.println("You pressed 3");
+                    break;
+                case 4:
+                    System.out.println("You pressed 4");
+                    break;
+                case 5:
+                    System.out.println("You pressed 5");
+                    break;
+                case 6:
+                    System.out.println("You pressed 6");
+                    break;
+                case 7:
+                    System.out.println("You pressed 7");
+                    break;
+                case 8:
+                    System.out.println("You pressed 8");
+                    quit = true;
+                    break;
+            }
+
+
         }
     }
 
