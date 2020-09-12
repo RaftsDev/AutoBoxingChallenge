f50bcfd7313fc11e373b951f25017e05abff95bd
Ralph F <RaftsDev@gmail.com>
2020-09-12 00:22:21 -0400

    Chore: Clean up.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -23,13 +23,8 @@ package com.company;
 // Think about where you are adding the code to perform certain actions
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Main {
-
-    //private static Scanner scanner = new Scanner(System.in);
-
-
 
     public static void main(String[] args) {
 
@@ -51,20 +46,15 @@ public class Main {
         addCustomer(bank, westernBranch, "Mike", 70);
         addCustomer(bank, southernBranch, "Tom", 50);
         addCustomer(bank, southernBranch, "Bill", 30);
+
         //Validation fake initial amount
         addCustomer(bank, westernBranch, "Donald", 0);
-
-        /*bank.addCustomer(westernBranch,"Bob",100.00);
-        bank.addCustomer(westernBranch,"Mike",70.00);
-        bank.addCustomer(southernBranch,"Tom",50.00);
-        bank.addCustomer(southernBranch,"Bill",30.00);*/
-        //Validation fake initial amount
-        //bank.addCustomer(southernBranch, "Donald", 0);
 
         //Additional transactions
 
         addTransaction(westernBranch,"Bob", -70.00);
         addTransaction(westernBranch,"Mike", -55.00);
+
         //Validation fake customer
         addTransaction(westernBranch,"Tom", -50.00);
 
@@ -72,91 +62,6 @@ public class Main {
         printBranchCustomers(westernBranch);
         printBranchCustomers(southernBranch);
 
-        // Add Customers to branches
-
-       /* westernBranch.addCustomer(Bob);
-        southernBranch.addCustomer(Tom);
-        westernBranch.addCustomer(Mike);
-        southernBranch.addCustomer(Bill);
-*/
-
-        //Print customers and transactions
-
-      /*  printBranchCustomers(westernBranch);
-        printBranchCustomers(southernBranch);*/
-
-        // Add transactions
-
-       /* Bob.addTransaction(bobDeposit);
-        Bob.addTransaction(bobCharge);*/
-
-
-        /*
-        boolean quit = false;
-        while(!quit){
-
-            int action = printOptions();
-            //System.out.println("action:"+action);
-
-            switch(action){
-                case 0://create a Branch
-                    System.out.println("You want to create a branch\n" +
-                            "type the name of branch:");
-                    String name = scanner.nextLine();
-                    Branch branchName = Branch.createBranch(name, bankName);
-                    bankName.addBranch(branchName);
-                    break;
-                case 1://create a Customer
-                    System.out.println("You want to create a customer\n" +
-                            "type the name of customer:");
-                    name = scanner.nextLine();
-                    System.out.println("Type the name of branch:");
-                    String tempBranchName = scanner.nextLine();
-
-
-                    break;
-                case 2:
-                    System.out.println("You pressed 2");
-                    break;
-                case 3://Show branches
-                    System.out.println("You branches:");
-                    ArrayList<Branch> branches = bankName.getBranches();
-                    //System.out.println("toString:"+branches.get(0).getName());
-                    for (int i = 0; i < branches.size(); i++){
-                        System.out.println(i+": "+branches.get(i).getName());
-                    }
-                    break;
-                case 4:
-                    System.out.println("You pressed 4");
-                    break;
-                case 5:
-                    System.out.println("You pressed 5");
-                    break;
-                case 6:
-                    System.out.println("You pressed 6");
-                    quit = true;
-                    break;
-            }
-
-
-        }
-    }
-
-    private static int printOptions(){
-        System.out.println("----------- List of available options --------");
-        System.out.println("0 - create a Branch\n" +
-                "1 - create a Customer\n" +
-                "2 - create a Transaction of customer\n" +
-                "3 - Show a Branches\n" +
-                "4 - Show a Customers of Branch\n"+
-                "5 - Show a Customers of Branch w transactions\n"+
-                "6 - Exit of application");
-
-        int action = scanner.nextInt();
-        scanner.nextLine();
-        return action;
-
-         */
     }
 
     public static void printBranches(Bank bank){
